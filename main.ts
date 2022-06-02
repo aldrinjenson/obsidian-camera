b4b51e643ccd78700202a8ec91121d2018aa71d8
Aldrin Jenson <aldrinjenson@gmail.com>
2022-06-02 21:13:25 +0530

    Fix distorted image issue

--- main.ts
+++ main.ts
@@ -1,66 +1,24 @@
-import {
-	App,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-} from "obsidian";
-
-interface ObsidianCameraSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: ObsidianCameraSettings = {
-	mySetting: "default",
-};
+import { App, MarkdownView, Modal, Notice, Plugin } from "obsidian";
 
 export default class ObsidianCamera extends Plugin {
-	settings: ObsidianCameraSettings;
-
 	async onload() {
-		await this.loadSettings();
 		console.log("loaded obsidian camera");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"camera",
-			"Obsidian Camera",
-			(evt: MouseEvent) => {
-				new SampleModal(this.app).open()
-			}
-		);
-		ribbonIconEl.addClass("my-plugin-ribbon-class"); // for css
+		this.addRibbonIcon("camera", "Obsidian Camera", (evt: MouseEvent) => {
+			new CameraModal(this.app).open();
+		});
 
 		this.addCommand({
 			id: "Open camera modal",
 			name: "Open camera modal",
 			callback: () => {
-				new SampleModal(this.app).open();
+				new CameraModal(this.app).open();
 			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
-	}
-
-	// onunload() {
-	// 	console.log("camera unloaded");
-	// }
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
+class CameraModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -73,24 +31,34 @@ class SampleModal extends Modal {
 		const { contentEl } = this;
 		const webCamContainer = contentEl.createDiv();
 		const videoEl = webCamContainer.createEl("video");
-		const recordVideoButton = webCamContainer.createEl("button");
-		const switchCameraButton = webCamContainer.createEl("button")
-		const snapPhotoButton = webCamContainer.createEl("button");
-		const canvas = webCamContainer.createEl("canvas");
-		canvas.style.display = "none";
-		recordVideoButton.innerText = "Start Recording";
-		// switchCameraButton.innerText = "Switch Camera";
-		snapPhotoButton.innerText = "Take a snap";
+		const recordVideoButton = webCamContainer.createEl("button", {
+			text: "Start recording",
+		});
+		const switchCameraButton = webCamContainer.createEl("button", {
+			text: "Switch Camera",
+		});
+		const snapPhotoButton = webCamContainer.createEl("button", {
+			text: "Take a snap",
+		});
+
+		const filePicker = webCamContainer.createEl("input", {
+			placeholder: "Choose image file from system",
+			type: "file",
+		});
+		filePicker.accept = "image/*";
+		filePicker.capture = "camera";
+		filePicker.onchange = async () => {
+			new Notice(filePicker.files[0].name); // to be removed
+			const chosenFile = filePicker.files[0];
+			const bufferFile = await chosenFile.arrayBuffer();
+			saveFile(bufferFile, true);
+		};
 
 		const chunks: BlobPart[] = [];
 		let recorder: MediaRecorder = null;
 		const chosenFolderPath = "attachments/snaps";
 
-
-
 		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-		const width = 500,
-			height = 100;
 
 		const saveFile = async (file: ArrayBuffer, isImage = false) => {
 			const dateString = (new Date() + "")
@@ -107,8 +75,7 @@ class SampleModal extends Modal {
 			const folderExists =
 				app.vault.getAbstractFileByPath(chosenFolderPath);
 			if (!folderExists) app.vault.createFolder(chosenFolderPath);
-			await app.vault.createBinary(filePath, file)
-
+			await app.vault.createBinary(filePath, file);
 
 			if (!view) return new Notice(`Video Saved to ${filePath}`);
 
@@ -129,29 +96,34 @@ class SampleModal extends Modal {
 			try {
 				return await navigator.mediaDevices.getUserMedia({
 					video: { deviceId: cameras[cameraIndex].deviceId },
-					// video: true,
 					audio: true,
 				});
 			} catch (error) {
 				console.log(error);
-				new Notice(error)
-				return null
+				new Notice(error);
+				return null;
 			}
+		};
 
-		}
+		const cameras = (
+			await navigator.mediaDevices.enumerateDevices()
+		).filter((d) => d.kind === "videoinput");
 
-		const cameras = (await navigator.mediaDevices.enumerateDevices()).filter(d => d.kind === 'videoinput')
-		if (cameras.length <= 1) switchCameraButton.style.display = 'none'
-		let cameraIndex = 0
+		if (cameras.length <= 1) switchCameraButton.style.display = "none";
+		let cameraIndex = 0;
 		switchCameraButton.onclick = async () => {
-			cameraIndex = (cameraIndex + 1) % cameras.length
-			console.log(cameras[cameraIndex])
-			videoStream = await getVideoStream()
-		}
+			cameraIndex = (cameraIndex + 1) % cameras.length;
+			videoStream = await getVideoStream();
+		};
 
-		// console.log(cameras)
 		const takepicture = () => {
-			videoEl.style.display = "none";
+			const canvas = webCamContainer.createEl("canvas");
+			canvas.style.display = "none";
+			let width = videoEl.videoWidth;
+			let height = videoEl.videoHeight;
+			canvas.height = height;
+			canvas.width = width;
+
 			canvas.getContext("2d").drawImage(videoEl, 0, 0, width, height);
 			canvas.toBlob(async (blob) => {
 				const bufferFile = await blob.arrayBuffer();
@@ -160,14 +132,14 @@ class SampleModal extends Modal {
 		};
 		let videoStream: MediaStream = null;
 
-		videoStream = await getVideoStream()
+		videoStream = await getVideoStream();
 
 		// if (!videoStream) return new Notice("Error in requesting video");
 		videoEl.srcObject = videoStream;
 
 		snapPhotoButton.onclick = takepicture;
 		recordVideoButton.onclick = async () => {
-			// switchCameraButton.disabled = true
+			switchCameraButton.disabled = true;
 			let isRecording: boolean =
 				recorder && recorder.state === "recording";
 			if (isRecording) recorder.stop();
@@ -203,31 +175,3 @@ class SampleModal extends Modal {
 	}
 }
 
-// class SampleSettingTab extends PluginSettingTab {
-// 	plugin: ObsidianCamera;
-
-// 	constructor(app: App, plugin: ObsidianCamera) {
-// 		super(app, plugin);
-// 		this.plugin = plugin;
-// 	}
-
-// 	display(): void {
-// 		const { containerEl } = this;
-// 		containerEl.empty();
-// 		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
-
-// 		new Setting(containerEl)
-// 			.setName("Video Save Path")
-// 			.setDesc("The folder path to which the videos will be saved")
-// 			.addText((text) =>
-// 				text
-// 					.setPlaceholder("Enter your secret")
-// 					.setValue(this.plugin.settings.mySetting)
-// 					.onChange(async (value) => {
-// 						console.log("save path: " + value);
-// 						this.plugin.settings.mySetting = value;
-// 						await this.plugin.saveSettings();
-// 					})
-// 			);
-// 	}
-// }
